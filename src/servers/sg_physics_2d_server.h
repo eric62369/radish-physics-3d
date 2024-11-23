21c95549c99a057bcbb71bd11cda5e13ddb28352
Eric Yoon <eyoon525@gmail.com>
2024-11-22 16:47:04 -0800

    imported sgphysics2d

--- src/servers/sg_physics_2d_server.h
+++ src/servers/sg_physics_2d_server.h
@@ -1,5 +1,28 @@
-#ifndef RD_PHYSICS_3D_SERVER_H
-#define RD_PHYSICS_3D_SERVER_H
+/*************************************************************************/
+/* Copyright (c) 2021-2022 David Snopek                                  */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef SG_PHYSICS_2D_SERVER_H
+#define SG_PHYSICS_2D_SERVER_H
 
 #include <godot_cpp/classes/node.hpp>
 #include <godot_cpp/classes/object.hpp>
@@ -7,16 +30,16 @@
 
 #include "../math/sg_fixed_transform_2d.h"
 
-class RDFixedNode3D;
-class RDCollisionObject3D;
-class RDPhysics3DServer;
+class SGFixedNode2D;
+class SGCollisionObject2D;
+class SGPhysics2DServer;
 
-class RDShape3DInternal;
-class RDCollisionObject3DInternal;
-class RDWorld3DInternal;
+class SGShape2DInternal;
+class SGCollisionObject2DInternal;
+class SGWorld2DInternal;
 
-class RDAreaCollision3D : public RefCounted {
-	GDCLASS(SGAreaCollision3D, RefCounted);
+class SGAreaCollision2D : public RefCounted {
+	GDCLASS(SGAreaCollision2D, RefCounted);
 
 	SGCollisionObject2D *collider;
 	RID collider_rid;
