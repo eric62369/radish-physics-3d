cd4262a886ed27d5c8d447d80422c84ab3a1596a
Eric Yoon <eyoon525@gmail.com>
2024-11-25 13:49:29 -0800

    trying to register new classes

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -12,17 +12,67 @@ using namespace godot;
 void initialize_gdextension_types(ModuleInitializationLevel p_level)
 {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
+		GDREGISTER_CLASS(SGFixed);
+		GDREGISTER_CLASS(SGFixedVector2);
+		GDREGISTER_CLASS(SGFixedRect2);
+		GDREGISTER_CLASS(SGFixedTransform2D);
+
+		GDREGISTER_CLASS(SGFixedNode2D);
+		GDREGISTER_CLASS(SGFixedPosition2D);
+		GDREGISTER_CLASS(SGPhysics2DServer);
+		GDREGISTER_VIRTUAL_CLASS(SGCollisionObject2D);
+		GDREGISTER_CLASS(SGArea2D);
+		GDREGISTER_CLASS(SGAreaCollision2D);
+		GDREGISTER_CLASS(SGPhysicsBody2D);
+		GDREGISTER_CLASS(SGStaticBody2D);
+		GDREGISTER_CLASS(SGCharacterBody2D);
+		GDREGISTER_CLASS(SGKinematicCollision2D);
+		GDREGISTER_CLASS(SGRayCast2D);
+		GDREGISTER_CLASS(SGRayCastCollision2D);
+
+		GDREGISTER_CLASS(SGCollisionShape2D);
+		GDREGISTER_CLASS(SGCollisionPolygon2D);
+
+		GDREGISTER_VIRTUAL_CLASS(SGShape2D);
+		GDREGISTER_CLASS(SGRectangleShape2D);
+		GDREGISTER_CLASS(SGCircleShape2D);
+		GDREGISTER_CLASS(SGCapsuleShape2D);
+
+		fixed_singleton = memnew(SGFixed);
+		Engine::get_singleton()->register_singleton("SGFixed", SGFixed::get_singleton());
+
+		server_singleton = memnew(SGPhysics2DServer);
+		Engine::get_singleton()->register_singleton("SGPhysics2DServer", SGPhysics2DServer::get_singleton());
 	}
 
 	GDREGISTER_CLASS(GDExample);
 	//GDREGISTER_CLASS(YourClass);
+#if defined(TOOLS_ENABLED) || defined(DEBUG_ENABLED)
+	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		GDREGISTER_CLASS(SGCollisionShape2DEditorPlugin);
+		GDREGISTER_CLASS(SGCollisionPolygon2DEditor);
+		GDREGISTER_CLASS(SGCollisionPolygon2DEditorPlugin);
+
+		EditorPlugins::add_by_type<SGCollisionShape2DEditorPlugin>();
+		EditorPlugins::add_by_type<SGCollisionPolygon2DEditorPlugin>();
+	}
+#endif
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+		Engine::get_singleton()->unregister_singleton("SGFixed");
+		memdelete(fixed_singleton);
+
+		Engine::get_singleton()->unregister_singleton("SGPhysics2DServer");
+		memdelete(server_singleton);
 	}
+
+#if defined(TOOLS_ENABLED) || defined(DEBUG_ENABLED)
+	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		EditorPlugins::remove_by_type<SGCollisionShape2DEditorPlugin>();
+	}
+#endif
 }
 
 extern "C"
